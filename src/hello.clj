a26c79789a922eea71abd3166d7e81588209d7e5
Juan Monetta <jpmonettas@gmail.com>
2024-07-24 15:27:15 -0300

    FlowStorm reload instructions (#1)

--- src/hello.clj
+++ src/hello.clj
@@ -2,6 +2,8 @@
   (:require [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]))
 
+(defonce server nil)
+
 (defn respond-hello [request]
   {:status 200 :body "Hello, world!"})
 
@@ -14,7 +16,32 @@
   (http/create-server
    {::http/routes routes
     ::http/type :jetty
+    ::http/join? false
     ::http/port 8890}))
 
 (defn start []
-  (http/start (create-server)))
+  (alter-var-root #'server (constantly (create-server)))
+  (http/start server))
+
+(defn stop []
+  (http/stop server)
+  (alter-var-root #'server (constantly nil)))
+
+
+(comment
+  ;; Start your server
+  (start)
+
+  ;; check it is working
+  ;; curl -i http://127.0.0.1:8890/greet
+
+  ;; Stop your server
+  (stop)
+
+  ;; Add instrumentation (with reload)
+
+  ;; Start your server again
+  (start)
+
+  ;; You need to re-create your server so it uses the new (instrumented) functions
+  )
